d275dd088ff32e21b901f2fdebc61411836ea1ad
Kid <xunxie@qq.com>
2016-04-01 16:40:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/kid/onekeylockscreen/AdminReceiver.java
@@ -0,0 +1,10 @@
+package com.example.kid.onekeylockscreen;
+
+import android.app.admin.DeviceAdminReceiver;
+
+/**
+ * Created by niuwa on 2016/4/1.
+ */
+public class AdminReceiver extends DeviceAdminReceiver {
+
+}
